e685a3b410fa6a1fb1bcbb76c274d2b6de056e1e
Kumar Appaiah <a.kumar@alumni.iitm.ac.in>
2021-10-27 09:41:01 +0530

    Add factorial function

--- main.c
+++ main.c
@@ -1,7 +1,18 @@
 #include <stdio.h>
 
 int
+factorial(int n)
+{
+	int product = 1;
+	for (int i = 1; i <= n; ++i)
+		product *= i;
+	return product;
+}
+
+int
 main(int argc, char *argv[])
 {
+	int n = 4;
+	printf("Factorial of %d is %d.\n", n, factorial(n));
 	return 0;
 }
