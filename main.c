b58dc2f53df6edee0d3dbf45d2ec3b7964ab6db2
Kumar Appaiah <a.kumar@alumni.iitm.ac.in>
2021-10-27 09:43:44 +0530

    Add is_even function

--- main.c
+++ main.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 
 int
+is_even(int i)
+{
+	return i % 2 == 0;
+}
+
+int
 factorial(int n)
 {
 	int product = 1;
@@ -12,7 +18,8 @@ factorial(int n)
 int
 main(int argc, char *argv[])
 {
-	int n = 4;
+	int n = 5;
 	printf("Factorial of %d is %d.\n", n, factorial(n));
+	printf("%d is even? Answer: %s\n", n, is_even(n) ? "Yes" : "No");
 	return 0;
 }
